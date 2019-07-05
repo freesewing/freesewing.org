305486233783944a6d9334193e8b4ab2951ca595
Joost De Cock <joost@decock.org>
2019-07-05 10:41:32 +0200

    :sparkles: Added docs index

--- src/components/templates/docs.js
+++ src/components/templates/docs.js
@@ -3,22 +3,26 @@ import MDXRenderer from "gatsby-mdx/mdx-renderer";
 import { MDXProvider } from '@mdx-js/react';
 import Blockquote from "@freesewing/components/Blockquote";
 import MeasurementsImages from "../measurements/images";
+import DocsIndexPage from "../docs/";
 
 const DocumentationPage = props => {
+  if (props.slug === "/docs") return <DocsIndexPage {...props} />
+
   const components = {
     Note: ({ children }) => { return <Blockquote type="note">{children}</Blockquote>},
     Tip: ({ children }) => { return <Blockquote type="tip">{children}</Blockquote>},
     Warning: ({ children }) => { return <Blockquote type="warning">{children}</Blockquote>},
   }
 
-  // Add measurements images when needed
   let prefix = null;
+  // Add measurements images when needed
   const chunks = props.slug.split("/");
   if (
     chunks.length === 4
     && chunks[1] === "docs"
     && chunks[2] === "measurements"
   ) prefix = <MeasurementsImages measurement={chunks[3]} />
+
 
   return (
     <React.Fragment>
