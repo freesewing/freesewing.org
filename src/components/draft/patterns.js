102569eff79b6f81e95ed805a1e898af346e478a
Joost De Cock <joost@decock.org>
2019-07-07 13:59:01 +0200

    :sparkles: Added the Benjamin pattern

--- src/components/draft/patterns.js
+++ src/components/draft/patterns.js
@@ -1,13 +1,16 @@
 import Aaron from "@freesewing/aaron";
+import Benjamin from "@freesewing/benjamin";
 import Bent from "@freesewing/bent";
 import Brian from "@freesewing/brian";
 import Bruce from "@freesewing/bruce";
 import Carlton from "@freesewing/carlton";
 import Carlita from "@freesewing/carlita";
 import Cathrin from "@freesewing/cathrin";
+import Florent from "@freesewing/florent";
 import Huey from "@freesewing/huey";
 import Hugo from "@freesewing/hugo";
 import Jaeger from "@freesewing/jaeger";
+import Shin from "@freesewing/shin";
 import Simon from "@freesewing/simon";
 import Sven from "@freesewing/sven";
 import Tamiko from "@freesewing/tamiko";
@@ -20,15 +23,18 @@ import Wahid from "@freesewing/wahid";
  */
 const patterns = {
   Aaron,
+  Benjamin,
   Bent,
   Brian,
   Bruce,
   Carlita,
   Carlton,
   Cathrin,
+  Florent,
   Huey,
   Hugo,
   Jaeger,
+  Shin,
   Simon,
   Sven,
   Tamiko,
