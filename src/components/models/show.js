bc7727bfbf778b541bbe1e733fef38985a072d0b
Joost De Cock <joost@decock.org>
2019-10-02 09:04:28 +0200

    Switched yellow color to orange
    This makes it consistent with the color on the edit measurement form. Also, the yellow is kinda hard to see on white.

--- src/components/models/show.js
+++ src/components/models/show.js
@@ -146,7 +146,7 @@ const ShowModel = ({ app, model }) => {
               {measurementInRange ? (
                 <ValidIcon size="small" style={{ color: '#40c057' }} data-test="valid" />
               ) : (
-                <InvalidIcon size="small" style={{ color: 'yellow' }} data-test="invalid" />
+                <InvalidIcon size="small" style={{ color: 'orange' }} data-test="invalid" />
               )}
             </>
           )}
