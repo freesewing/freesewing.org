c36a8a52b1c005bd92cba3edfa30a67005cf1b73
Joost De Cock <joost@decock.org>
2020-02-20 18:14:43 +0100

    feat: ReadMore can recurse now

--- src/components/read-more.js
+++ src/components/read-more.js
@@ -2,10 +2,12 @@ import React from 'react'
 import useApp from '../hooks/useApp'
 import useNavigation from '../hooks/useNavigation'
 import { Link } from 'gatsby'
+import Blockquote from '@freesewing/components/Blockquote'
+import { FormattedMessage } from 'react-intl'
 
 // FIXME: This only handles second-level and third-level docs pages for now
 
-const ReadMore = props => {
+const ReadMore = ({ root, recurse=false, link=false, box=false }) => {
   const app = useApp()
   const { tree, titles } = useNavigation(app)
 
@@ -19,18 +21,37 @@ const ReadMore = props => {
     return {}
   }
 
-  let children = getChildren(props.root)
+  const renderDocs = root => {
+    let children = getChildren(root)
+    let links = []
+    for (let slug in children) links.push(renderDocsLevel(slug, children[slug]))
 
+    return <ul className='links'>{links}</ul>
+  }
 
-  return (
-    <ul className='links'>
-      {Object.keys(children).map( slug => (
-        <li key={slug}>
-          <Link to={slug}>{children[slug].title}</Link>
-        </li>
-      ))}
-    </ul>
-  )
+  const renderDocsLevel = (slug, level) => {
+    let links = []
+    if (level.children && Object.keys(level.children).length > 0 && recurse) {
+      for (let slug in level.children) links.push(renderDocsLevel(slug, level.children[slug]))
+    }
+    if (links.length > 0) links = <ul className='links'>{links}</ul>
+    let url = null
+    if (link) url = <span style={styles.url}>{slug}</span>
+
+    return <li><Link to={slug}>{titles[slug]}{url}</Link>{links}</li>
+  }
+
+  const styles = {
+    url: {
+      display: 'block',
+      margin: '0 0 0.25rem 1rem',
+      fontSize: '0.9rem',
+      color: app.theme === 'dark' ? 'white' : 'black',
+    },
+  }
+
+  if (box) return <Blockquote type='note'><h5><FormattedMessage id='app.furtherReading' /></h5>{renderDocs(root)}</Blockquote>
+  else return renderDocs(root)
 }
 
 export default ReadMore
