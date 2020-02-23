64fb0574401af09fd541a05cc6f1622d95048ee3
Joost De Cock <joost@decock.org>
2020-02-23 10:55:29 +0100

    fix: Switched to new YouTube component

--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -4,13 +4,15 @@ import { MDXProvider } from '@mdx-js/react'
 
 import Blockquote from '@freesewing/components/Blockquote'
 import ReadMore from '../read-more'
+import YouTube from '../youtube'
 
 const customComponents = {
   Note: ({ children }) => <Blockquote type="note">{children}</Blockquote>,
   Tip: ({ children }) => <Blockquote type="tip">{children}</Blockquote>,
   Warning: ({ children }) => <Blockquote type="warning">{children}</Blockquote>,
   Fixme: ({ children }) => <Blockquote type="fixme">{children}</Blockquote>,
-  ReadMore: props => <ReadMore {...props} />
+  ReadMore: props => <ReadMore {...props} />,
+  YouTube
 }
 
 const Mdx = ({ node }) => (
---
+++ src/components/youtube.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const YouTube = props => {
+  console.log(props)
+  return(
+  <div
+    className="video"
+    style={{
+       position: "relative",
+       paddingBottom: "56.25%" /* 16:9 */,
+       paddingTop: 25,
+       height: 0
+    }}
+  >
+    <iframe
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%"
+      }}
+      src={props.playlist
+        ? `https://www.youtube.com/embed/videoseries?list=${props.id}`
+        : `https://www.youtube.com/embed/${props.id}`
+      }
+      frameBorder="0"
+    />
+  </div>
+);
+}
+export default YouTube
