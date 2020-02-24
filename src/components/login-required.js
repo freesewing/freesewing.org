6ea1663cd248b269406b0809c7c6f8f2aac2f84e
Joost De Cock <joost@decock.org>
2020-02-24 18:28:23 +0100

    fix: Don't pass intent to login as we don't use it

--- src/components/login-required.js
+++ src/components/login-required.js
@@ -2,15 +2,9 @@ import { navigate } from 'gatsby'
 
 const LoginRequired = ({ app, children }) => {
   if (app.account.username && app.account.status === 'active') return children
-  else {
-    if (typeof window !== 'undefined') {
-      // Only try this in the browser
-      navigate('/login', {
-        state: {
-          intent: props.page
-        }
-      })
-    }
+  else if (typeof window !== 'undefined') {
+    // Only try this in the browser
+    navigate('/login/')
 
     return null
   }
