fc893a202d48ede191cf531b5f247241db8e06c4
Joost De Cock <joost@decock.org>
2020-02-25 08:07:37 +0100

    fix: Issue in login-required component not returning

--- src/components/login-required.js
+++ src/components/login-required.js
@@ -2,9 +2,11 @@ import { navigate } from 'gatsby'
 
 const LoginRequired = ({ app, children }) => {
   if (app.account.username && app.account.status === 'active') return children
-  else if (typeof window !== 'undefined') {
-    // Only try this in the browser
-    navigate('/login/')
+  else {
+    if (typeof window !== 'undefined') {
+      // Only try this in the browser
+      navigate('/login')
+    }
 
     return null
   }
