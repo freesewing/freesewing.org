230d0df65ddb5068be041fcb80559d8eef25b869
Joost De Cock <joost@decock.org>
2020-04-11 13:06:28 +0200

    chore: Simplified pattern menu

--- src/components/menus/patterns.js
+++ src/components/menus/patterns.js
@@ -32,9 +32,8 @@ const PatternMenu = ({ app }) => {
     button: {
       margin: '0.5rem 0.5rem 0.5rem 0'
     },
-    doubleButton: {
-      margin: '1.5rem 0.5rem 0.5rem',
-      width: 'calc(50% - 1rem)'
+    topButtons: {
+      margin: '2rem 0.5rem 0.5rem 0.5rem'
     }
   }
 
@@ -43,19 +42,24 @@ const PatternMenu = ({ app }) => {
       <div style={style.col}>
         {!app.mobile && (
           <>
-            <Button variant="contained" color="primary" style={style.doubleButton} href="/create/">
+            <Button variant="contained" color="primary" style={style.topButtons} href="/designs/">
+              <FormattedMessage id="app.browseCollection" />
+            </Button>
+            {app.account.username && (
+              <Button variant="outlined" color="primary" style={style.topButtons} href="/patterns/">
+                <FormattedMessage id="app.browseYourPatterns" />
+              </Button>
+            )}
+            <Button color="primary" style={style.topButtons} href="/create/">
               <FormattedMessage
                 id="app.newThing"
                 values={{ thing: app.translate('app.pattern') }}
               />
             </Button>
-            <Button variant="outlined" color="primary" style={style.doubleButton} href="/designs/">
-              <FormattedMessage id="app.browseCollection" />
-            </Button>
           </>
         )}
         <h6>
-          <FormattedMessage id="app.availablePatterns" />
+          <FormattedMessage id="app.designs" />
         </h6>
         <ul style={style.patterns}>
           {patterns.map((pattern, index, array) => (
@@ -63,66 +67,24 @@ const PatternMenu = ({ app }) => {
               <Link to={`/designs/${pattern}/`} title={app.translate(`patterns.${pattern}.title`)}>
                 {app.translate(`patterns.${pattern}.title`)}
               </Link>
-              { index === (array.length - 1) ? null : ', '}
+              {index === array.length - 1 ? null : ', '}
             </li>
           ))}
         </ul>
       </div>
       <div style={style.col}>
-        <h6>
-          <FormattedMessage id="app.yourPatterns" />
-        </h6>
-        {app.account.username ? (
-          Object.keys(app.patterns).length > 0 ? (
-            <>
-              <ul style={style.patterns}>
-                {Object.keys(app.patterns).map((handle) => (
-                  <li key={handle} style={style.pattern}>
-                    <Link to={`/patterns/${handle}/`}>{app.patterns[handle].name}</Link>
-                  </li>
-                ))}
-              </ul>
-              <p>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  style={{ ...style.button }}
-                  href="/patterns/"
-                >
-                  <FormattedMessage id="app.browseYourPatterns" />
-                </Button>
-              </p>
-            </>
-          ) : (
-            <p>
-              <FormattedMessage id="app.noPattern" />
-              <br />
-              <Button variant="contained" color="primary" style={style.button} href="/create/">
-                <FormattedMessage
-                  id="app.newThing"
-                  values={{ thing: app.translate('app.pattern') }}
-                />
-              </Button>
-            </p>
-          )
-        ) : (
+        {app.account.username && Object.keys(app.patterns).length > 0 && (
           <>
-            <p>
-              <FormattedMessage id="app.loginNeededToSavePatternsMsg" />
-            </p>
-            <p>
-              <Button variant="contained" color="primary" style={style.button} href="/login">
-                <FormattedMessage id="app.logIn" />
-              </Button>
-              <Button
-                variant="outlined"
-                color="primary"
-                style={{ ...style.button, marginLeft: '1rem' }}
-                href="/signup/"
-              >
-                <FormattedMessage id="app.signUp" />
-              </Button>
-            </p>
+            <h6>
+              <FormattedMessage id="app.yourPatterns" />
+            </h6>
+            <ul style={style.patterns}>
+              {Object.keys(app.patterns).map((handle) => (
+                <li key={handle} style={style.pattern}>
+                  <Link to={`/patterns/${handle}/`}>{app.patterns[handle].name}</Link>
+                </li>
+              ))}
+            </ul>
           </>
         )}
       </div>
