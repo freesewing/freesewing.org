0a8e6029c21a081d14113e7b33ab8368d245b296
Joost De Cock <joost@decock.org>
2020-04-13 13:39:09 +0200

    fix: Issue in size component

--- src/components/size.js
+++ src/components/size.js
@@ -5,34 +5,29 @@ import { Link } from 'gatsby'
 import formatMm from '@freesewing/utils/formatMm'
 import convertSize from '@freesewing/utils/convertSize'
 
-const Size = (props) => {
-  const convertSize = (breasts, size) =>
-    breasts ? sizeConversion.withBreasts[size] : sizeConversion.withoutBreasts[size]
-
-  return (
-    <div className={`size ${props.breasts ? 'breasts' : 'no-breasts'}`}>
-      <div className="top">
-        <h5>
-          <Icon icon={props.breasts ? 'withBreasts' : 'withoutBreasts'} size={52} />
-          <span className="prefix">{props.translate('app.size')}</span>
-          <span className="size">{convertSize(props.breasts, props.size)}</span>
-        </h5>
-      </div>
-      <div className="bottom">
-        {props.translate('measurements.neckCircumference')}
-        <br />
-        <span className="val">{props.size}cm</span>
-        |
-        <span
-          className="val"
-          dangerouslySetInnerHTML={{
-            __html: '<b>' + formatMm(props.size * 10, 'imperial', 'html') + '</b>'
-          }}
-        />
-      </div>
-      <Link to={props.link} className="link" />
+const Size = (props) => (
+  <div className={`size ${props.breasts ? 'breasts' : 'no-breasts'}`}>
+    <div className="top">
+      <h5>
+        <Icon icon={props.breasts ? 'withBreasts' : 'withoutBreasts'} size={52} />
+        <span className="prefix">{props.translate('app.size')}</span>
+        <span className="size">{convertSize(props.size, props.breasts)}</span>
+      </h5>
     </div>
-  )
-}
+    <div className="bottom">
+      {props.translate('measurements.neckCircumference')}
+      <br />
+      <span className="val">{props.size}cm</span>
+      |
+      <span
+        className="val"
+        dangerouslySetInnerHTML={{
+          __html: '<b>' + formatMm(props.size * 10, 'imperial', 'html') + '</b>'
+        }}
+      />
+    </div>
+    <Link to={props.link} className="link" />
+  </div>
+)
 
 export default React.memo(Size)
