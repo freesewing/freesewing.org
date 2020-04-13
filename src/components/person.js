15115b74e32b1ba3334e752c62c726f0fa904c73
Joost De Cock <joost@decock.org>
2020-04-13 15:39:58 +0200

    fix: Handled SSR in person component

--- src/components/person.js
+++ src/components/person.js
@@ -3,32 +3,37 @@ import './person.scss'
 import Icon from '@freesewing/components/Icon'
 import { Link } from 'gatsby'
 
-const Person = (props) => (
-  <div className={`person ${props.data.breasts ? 'breasts' : 'no-breasts'}`}>
-    <div className="top">
-      <h5>{props.data.name}</h5>
+const Person = (props) => {
+  // SSR
+  if (!props.data) return null
+
+  return (
+    <div className={`person ${props.data.breasts ? 'breasts' : 'no-breasts'}`}>
+      <div className="top">
+        <h5>{props.data.name}</h5>
+      </div>
+      <div className="bottom">
+        <ul>
+          <li>
+            <Icon icon="units" /> {props.translate(`app.${props.data.units}Units`)}
+          </li>
+          <li>
+            <Icon icon="measurements" />{' '}
+            {props.data.measurements ? Object.keys(props.data.measurements).length : 0}{' '}
+            {props.translate('app.measurements')}
+          </li>
+          <li>
+            <Icon icon={props.data.breasts ? 'withBreasts' : 'withoutBreasts'} />
+            {props.translate(props.data.breasts ? 'app.withBreasts' : 'app.withoutBreasts')}
+          </li>
+        </ul>
+      </div>
+      <div className="avatar">
+        <img src={props.data.pictureUris.m} />
+      </div>
+      <Link to={props.link} className="link" />
     </div>
-    <div className="bottom">
-      <ul>
-        <li>
-          <Icon icon="units" /> {props.translate(`app.${props.data.units}Units`)}
-        </li>
-        <li>
-          <Icon icon="measurements" />{' '}
-          {props.data.measurements ? Object.keys(props.data.measurements).length : 0}{' '}
-          {props.translate('app.measurements')}
-        </li>
-        <li>
-          <Icon icon={props.data.breasts ? 'withBreasts' : 'withoutBreasts'} />
-          {props.translate(props.data.breasts ? 'app.withBreasts' : 'app.withoutBreasts')}
-        </li>
-      </ul>
-    </div>
-    <div className="avatar">
-      <img src={props.data.pictureUris.m} />
-    </div>
-    <Link to={props.link} className="link" />
-  </div>
-)
+  )
+}
 
 export default React.memo(Person)
