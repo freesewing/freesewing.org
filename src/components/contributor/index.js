3fe7a41d717439d28afc527632fa558120f986ed
Joost De Cock <joost@decock.org>
2020-09-21 13:03:36 +0200

    chore: Updated contributor component

--- src/components/contributor/index.js
+++ src/components/contributor/index.js
@@ -8,6 +8,12 @@ const Contributor = ({ contributor, app }) => (
     <div className="text">
       <h5>{contributor.name}</h5>
       <ul>
+        <li className="github">
+          <a href={`https://github.com/${contributor.github}`}>@{contributor.github}</a>
+        </li>
+        <li className="discord">@{contributor.discord}</li>
+      </ul>
+      <ul>
         {contributor.tags.map((tag) => (
           <li>
             <FormattedMessage id={`cty.${tag}`} />
