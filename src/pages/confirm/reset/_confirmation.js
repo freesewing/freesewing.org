42a0dc64a0e7950235f416c175cffe95c11d3ac6
Joost De Cock <joost@decock.org>
2020-10-03 13:00:27 +0200

    fix: Fixed unused context

--- src/pages/confirm/reset/_confirmation.js
+++ src/pages/confirm/reset/_confirmation.js
@@ -2,19 +2,14 @@ import React, { useEffect } from 'react'
 import useApp from '../../../hooks/useApp'
 import AppWrapper from '../../../components/app/wrapper'
 
-const Page = props => {
-
+const Page = (props) => {
   const app = useApp()
 
   useEffect(() => {
     app.confirmationLogin(props.confirmation)
   }, [])
 
-  return <AppWrapper
-    app={app}
-    title={app.translate('app.justAMoment')}
-    active='designs'
-  />
+  return <AppWrapper app={app} title={app.translate('app.justAMoment')} active="designs" />
 }
 
 export default Page
