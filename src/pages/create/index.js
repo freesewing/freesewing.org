caea21c72b2b0d231fef2f4005f04cf32014e4d1
Joost De Cock <joost@joost.at>
2021-01-03 15:09:28 +0100

    wip: Updated design picker

--- src/pages/create/index.js
+++ src/pages/create/index.js
@@ -7,94 +7,32 @@ import { Link } from 'gatsby'
 import { list } from '@freesewing/pattern-info'
 import LineDrawing from '@freesewing/components/LineDrawing'
 import capitalize from '@freesewing/utils/capitalize'
+import './index.scss'
 
 const Page = (props) => {
   const app = useApp()
-
-  const styles = {
-    wrapper: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center'
-    },
-    post: {
-      width: '200px',
-      margin: '6px',
-      padding: '1rem',
-      textAlign: 'center',
-      background: app.theme === 'light' ? '#f1f3f5' : '#343a40'
-    },
-    figure: {
-      margin: 0,
-      padding: 0
-    },
-    title: {
-      border: 0,
-      fontSize: '1.5rem',
-      margin: 0,
-      padding: 0,
-      lineHeight: 1.25
-    },
-    blurb: {
-      fontSize: '1.1rem',
-      margin: 0,
-      padding: 0
-    },
-    link: {
-      color: 'inherit',
-      textDecoration: 'none'
-    },
-    img: {
-      borderRadius: '4px',
-      width: '100%'
-    }
-  }
-  if (app.mobile) {
-    styles.post.width = '125px'
-    styles.post.padding = '0.5rem'
-  }
-
-  const context = [
-    <h5>
-      <FormattedMessage id="app.designs" />
-    </h5>,
-    <ul>
-      {list.map((pattern) => (
-        <li key={pattern}>
-          <Link to={`/create/${pattern}/`}>{app.translate(`patterns.${pattern}.title`)}</Link>
-        </li>
-      ))}
-    </ul>
-  ]
 
   return (
     <AppWrapper
       app={app}
       title={app.translate('app.chooseADesign')}
-      context={context}
       crumbs={[
         {
           slug: '/create/',
           title: app.translate('app.newThing', { thing: app.translate('app.pattern') })
         }
       ]}
-      active="designs"
+      wide
     >
-      <div style={styles.wrapper}>
+      <div id="design-wrapper">
         {list.map((pattern) => {
           let title = app.translate(`patterns.${pattern}.title`)
           return (
-            <div key={pattern} style={styles.post} className="shadow">
-              <Link
-                data-test="post-link"
-                to={`/create/${pattern}/`}
-                style={styles.link}
-                title={title}
-              >
-                <LineDrawing pattern={pattern} size={app.mobile ? 92 : 164} />
-                <h2 style={styles.title}>{capitalize(pattern)}</h2>
-                <p style={styles.blurb}>
+            <div key={pattern} className="shadow designcard">
+              <Link data-test="post-link" to={`/create/${pattern}/`} title={title}>
+                <LineDrawing pattern={pattern} size={166} />
+                <h5>{capitalize(pattern)}</h5>
+                <p>
                   <FormattedMessage id={'patterns.' + pattern + '.title'} />
                 </p>
               </Link>
