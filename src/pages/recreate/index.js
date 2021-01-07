c2a7e1aa5795d7e6861f9ff276d9dc6329d84341
Joost De Cock <joost@joost.at>
2021-01-07 14:33:57 +0100

    fix: Recreate page as create page

---
+++ src/pages/recreate/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import useApp from '../../hooks/useApp'
+import AppWrapper from '../../components/app/wrapper'
+
+import { FormattedMessage } from 'react-intl'
+import { Link } from 'gatsby'
+import { list } from '@freesewing/pattern-info'
+
+const Page = (props) => {
+  const app = useApp()
+
+  return (
+    <AppWrapper
+      app={app}
+      title={app.translate('app.chooseADesign')}
+      crumbs={[
+        {
+          slug: '/create/',
+          title: app.translate('app.newThing', { thing: app.translate('app.pattern') })
+        }
+      ]}
+    >
+      <ul className="links">
+        {list.map((pattern) => {
+          let title = app.translate(`patterns.${pattern}.title`)
+          return (
+            <li key={pattern}>
+              <Link to={`/create/${pattern}/`} title={title}>
+                <FormattedMessage id={'patterns.' + pattern + '.title'} />
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </AppWrapper>
+  )
+}
+
+export default Page
