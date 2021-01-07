bdb8d67de3cc7498ae07c624ac9b3c3d53cc662b
Joost De Cock <joost@joost.at>
2021-01-07 13:46:13 +0100

    chore: Tweak to sizing docs

--- src/pages/sizes.js
+++ src/pages/sizes.js
@@ -1,56 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useApp from '../hooks/useApp'
 import AppWrapper from '../components/app/wrapper'
 
 import SizingTable from '../components/size-table'
 import SizingGraph from '../components/person/size-graph'
-import { FormattedMessage } from 'react-intl'
+import BreadCrumbs from '../components/breadcrumbs'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
+import Icon from '@freesewing/components/Icon'
 
 const Page = (props) => {
   const app = useApp(false)
 
-  const context = [
-    <h5>
-      <FormattedMessage id="app.sizes" />
-    </h5>,
-    <ul>
-      <li>
-        <a href="#with">
-          <FormattedMessage id="app.withBreasts" />
-        </a>
-      </li>
-      <li>
-        <a href="#without">
-          <FormattedMessage id="app.withoutBreasts" />
-        </a>
-      </li>
-    </ul>
-  ]
+  const [breasts, setBreasts] = useState(true)
+  const [chart, setChart] = useState(false)
+  const title = app.translate('app.sizes')
 
   return (
-    <AppWrapper app={app} title={app.translate('app.sizes')} context={context}>
-      <ul className="links">
-        <li>
-          <a href="#with">
-            <FormattedMessage id="app.withBreasts" />
-          </a>
-        </li>
-        <li>
-          <a href="#without">
-            <FormattedMessage id="app.withoutBreasts" />
-          </a>
-        </li>
-      </ul>
-      <h2 id="with">
-        <FormattedMessage id="app.withBreasts" />
-      </h2>
-      <SizingGraph breasts={true} app={app} />
-      <SizingTable breasts={true} />
-      <h2 id="without">
-        <FormattedMessage id="app.withoutBreasts" />
-      </h2>
-      <SizingGraph breasts={false} app={app} />
-      <SizingTable breasts={false} />
+    <AppWrapper app={app} title={title} noLayout>
+      <div
+        style={{
+          maxWidth: '1600px',
+          width: '100%',
+          margin: '0 auto',
+          paddingBottom: '3rem'
+        }}
+      >
+        <BreadCrumbs app={props.app} pageTitle={title} />
+        <h1>{title}</h1>
+        <div
+          style={{
+            maxWidth: '600px',
+            width: '100%',
+            margin: '0',
+            padding: '1rem'
+          }}
+        >
+          <FormControlLabel
+            control={
+              <Switch
+                checked={breasts}
+                onChange={() => setBreasts(!breasts)}
+                name="breasts"
+                inputProps={{ 'aria-label': app.translate('app.breasts') }}
+                color="primary"
+              />
+            }
+            label={<Icon icon={breasts ? 'withBreasts' : 'withoutBreasts'} />}
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={chart}
+                onChange={() => setChart(!chart)}
+                name="chart"
+                inputProps={{ 'aria-label': '📉' }}
+                color="primary"
+              />
+            }
+            label={chart ? '📉' : '📄'}
+          />
+        </div>
+        {chart ? <SizingGraph breasts={breasts} app={app} /> : <SizingTable breasts={breasts} />}
+      </div>
     </AppWrapper>
   )
 }
