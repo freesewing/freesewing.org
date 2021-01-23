5baaf2a307543f0eacd8536675016e12a19e08c9
Joost De Cock <joost@joost.at>
2021-01-23 16:26:55 +0100

    fix: Blog post tests

--- cypress/integration/blog.spec.js
+++ cypress/integration/blog.spec.js
@@ -1,27 +1,58 @@
-// FIXME: This should obviously be handled in logic,
-// but doing so makes it hard to run a test per post :|
-const posts = [
-  '/blog/roundup-2019-01',
-  '/blog/core-1-8-jaeger-across-back',
-  '/blog/email-spam-problems',
-  '/blog/freesewing-goes-jamstack'
-]
+describe('Blog posts', () => {
+  before(() => {
+    // Get latest blog posts from the homepage
+    cy.visit('/')
+    cy.get('div#blog div.single div.post > a.image').click({ force: true })
+  })
 
-describe('Blog posts', function () {
-  for (let post of posts) {
-    it(post, function () {
-      if (1 || post === '/blog/roundup-2017-09') {
-        cy.visit(post)
-        cy.get('h1').should('not.be.empty')
-        cy.get('div[data-test=meta] > span').should('not.be.empty')
-        cy.get('a[data-test=author]').should('not.be.empty')
-        cy.get('article img').should('have.attr', 'alt')
-        cy.get('article img').each(($el, index, $list) => {
-          if ($el.attr('src').slice(0, 4) !== 'data') {
-            cy.request($el.attr('src')).its('status').should('equal', 200)
-          }
-        })
-      }
+  describe('Verify blog post', () => {
+    it('Heading should be present', () => {
+      cy.get('h1').should('not.be.empty')
     })
-  }
+    it('Date should be present', () => {
+      cy.get('[data-test=meta').should('contain', ', 202')
+    })
+    it('Author should be present', () => {
+      cy.get('a[data-test=author]').should('not.be.empty')
+    })
+    it('Author should link to a user profile', () => {
+      cy.get('a[data-test=author]').invoke('attr', 'href').should('contain', '/users/')
+    })
+    it('Image should have a src', () => {
+      cy.get('img[data-test=img]').invoke('attr', 'src').should('contain', '/static/')
+    })
+    it('Image should have a srcSet', () => {
+      cy.get('img[data-test=img]').invoke('attr', 'srcset').should('not.be.empty')
+    })
+    it('Image should have an alt', () => {
+      cy.get('img[data-test=img]').invoke('attr', 'alt').should('not.be.empty')
+    })
+    it('Figure should have a caption', () => {
+      cy.get('figcaption[data-test=caption]').should('not.be.empty')
+    })
+    it('Post should have content', () => {
+      cy.get('#mdx').should('not.be.empty')
+    })
+    it('All images should load', () => {
+      cy.get('div.content img').each(($el, index, $list) => {
+        if ($el.attr('src').slice(0, 4) !== 'data') {
+          cy.request($el.attr('src')).its('status').should('equal', 200)
+        }
+      })
+    })
+  })
+
+  describe('Verify navigation', () => {
+    it('Blog section should be active', () => {
+      cy.get('#main-menu > li > a.active').invoke('attr', 'href').should('equal', '/blog/')
+    })
+    it('Article link should be active', () => {
+      cy.get('#main-menu > li > a.active + ul.level-1 > li > a.active')
+        .invoke('attr', 'href')
+        .should('contain', '/blog/')
+    })
+    it('Navigation should have links to many blog posts', () => {
+      cy.get('#main-menu ul.level-1 li').its('length').should('gt', 20)
+    })
+  })
 })
