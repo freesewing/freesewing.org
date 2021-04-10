86a5e5900e0163ef8f064a0093364db1cc53da9a
Joost De Cock <joost@joost.at>
2021-04-10 10:33:49 +0200

    fix: Avoid use of symbolic links for Windows users

--- src/pages/confirm/email/_confirmation.js
+++ src/pages/confirm/email/_confirmation.js
@@ -1,1 +1,15 @@
-../reset/_confirmation.js
+import React, { useEffect } from 'react'
+import useApp from '../../../hooks/useApp'
+import AppWrapper from '../../../components/app/wrapper'
+
+const Page = (props) => {
+  const app = useApp()
+
+  useEffect(() => {
+    app.confirmationLogin(props.confirmation)
+  }, [])
+
+  return <AppWrapper app={app} title={app.translate('app.justAMoment')} active="designs" />
+}
+
+export default Page
